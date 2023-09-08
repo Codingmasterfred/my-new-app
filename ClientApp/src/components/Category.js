bd6b0c7d5c0203daf635ebd62221459a914a2285
Codingmasterfred <fredrickg777@gmail.com>
2023-09-08 10:03:23 -0700

    all routes done

--- ClientApp/src/components/Category.js
+++ ClientApp/src/components/Category.js
@@ -5,9 +5,23 @@ import axios from 'axios';
 import Nav from "./NavMenu"
 import SecondNavBar from "./SecondNavBar"
 import Offcanvas from 'react-bootstrap/Offcanvas';
+import Form from 'react-bootstrap/Form';
+    
+    import Modal from 'react-bootstrap/Modal';
 
 function CategoryPage(props) {
     const [show, setShow] = useState(false);
+    const [productName, setProductName] = useState('');
+    const [productDescription, setProductDescription] = useState('');
+    const [productPrice, setProductPrice] = useState("")
+    const [updatedProductName, setUpdatedProductName] = useState('');
+    const [updatedProductDescription, setUpdatedProductDescription] = useState('');
+    const [updatedProductPrice, setUpdatedProductPrice] = useState('');
+    const [shows, setShows] = useState(false);
+    const [productId,setProductId] = useState("")
+
+
+
 
     const loadClickedItemData = () => {
         // Check if a clicked item exists in local storage
@@ -30,6 +44,9 @@ function CategoryPage(props) {
     const handleClose = () => setShow(false);
     const handleShow = () => setShow(true);
 
+    const handleCloses = () => setShows(false);
+    const handleShows = () => setShows(true);
+
     useEffect(() => {
         // Save the latest clicked item to local storage
         if (props.clickedItem.id) {
@@ -44,6 +61,62 @@ function CategoryPage(props) {
         }
     }, [props.clickedItem]);
 
+    function handleCreateProduct() {
+        axios.post("https://localhost:7260/Products", {
+            name: productName,
+            description: productDescription,
+            price: productPrice,
+            categoryId: props.clickedItem.id
+        })
+            .then(res => {
+                console.log("We updated it ", res)
+                window.location.reload()
+
+            })
+            .catch(res => {
+                console.error("failed", res)
+            })
+
+    }
+
+    function handleDeleteRequest(id) {
+        axios.delete("https://localhost:7260/Products/" + id)
+            .then(res => {
+                console.log("We updated it ", res)
+                window.location.reload()
+
+            })
+            .catch(res => {
+                console.error("failed", res)
+            })
+
+    }
+
+    function handleUpdateProductId(id) {
+        handleShows(true)
+        setProductId(id)
+    }
+
+    function handleUpdateProduct() {
+        axios.put(`https://localhost:7260/Products/${productId}`, {
+            name: updatedProductName,
+            description: updatedProductDescription,
+            price: updatedProductPrice,
+            categoryId: props.clickedItem.id
+        })
+            .then(res => {
+                console.log("Product updated successfully", res);
+        handleCloses(true)
+                window.location.reload();
+            })
+            .catch(error => {
+                console.error("Failed to update product", error);
+            });
+
+    }
+
+
+
     return (
         <>
             <Nav />
@@ -51,7 +124,50 @@ function CategoryPage(props) {
             <div style={{ height: "100vh", width: "100%", display: "flex", justifyContent: "space-around" }}>
                 <SecondNavBar
                     setClickedItem={props.setClickedItem} Category={props.Category} setCategories={props.setCategories} ClickedItem={props.ClickedItem} />
-                <div id="ContainProducts" style={{ display: "flex", flexDirection: "column" }}>
+                <div id="ContainProducts" style={{ display: "flex", flexDirection: "column", }}>
+                    <Modal show={shows} onHide={handleCloses}>
+                        <Modal.Header closeButton>
+                            <Modal.Title>Modal heading</Modal.Title>
+                        </Modal.Header>
+                        <Modal.Body> <Form>
+                            <Form.Group className="mb-3">
+                                <Form.Label>Product Name</Form.Label>
+                                <Form.Control
+                                    type="text"
+                                    placeholder="Enter product name"
+
+                                    onChange={(e) => setUpdatedProductName(e.target.value)}
+                                />
+                            </Form.Group>
+                            <Form.Group className="mb-3">
+                                <Form.Label>Product Description</Form.Label>
+                                <Form.Control
+                                    type="text"
+                                    placeholder="Enter product description"
+
+                                    onChange={(e) => setUpdatedProductDescription(e.target.value)}
+                                />
+                            </Form.Group>
+                            <Form.Group className="mb-3">
+                                <Form.Label>Product Price</Form.Label>
+                                <Form.Control
+                                    type="number"
+                                    placeholder="Enter product price"
+
+                                    onChange={(e) => setUpdatedProductPrice(e.target.value)}
+                                />
+                            </Form.Group>
+                            </Form>
+                        </Modal.Body>
+                        <Modal.Footer>
+                            <Button variant="secondary" onClick={handleCloses}>
+                                Close
+                            </Button>
+                            <Button variant="primary" onClick={handleUpdateProduct }>
+                                Update
+                            </Button>
+                        </Modal.Footer>
+                    </Modal>
                     <div style={{ display: "flex", justifyContent: "space-evenly" }}>
                         <h1 style={{}}>{props.clickedItem.name}</h1>
                         {props.clickedItem.name && (
@@ -64,15 +180,46 @@ function CategoryPage(props) {
                                 <Offcanvas.Title>Create A Product</Offcanvas.Title>
                             </Offcanvas.Header>
                             <Offcanvas.Body>
-                                Some text as a placeholder. In real life, you can have the elements you
-                                have chosen. Like text, images, lists, etc.
+                                <Form>
+                                    <Form.Group className="mb-3">
+                                        <Form.Label>Product Name</Form.Label>
+                                        <Form.Control
+                                            type="text"
+                                            placeholder="Enter product name"
+
+                                            onChange={(e) => setProductName(e.target.value)}
+                                        />
+                                    </Form.Group>
+                                    <Form.Group className="mb-3">
+                                        <Form.Label>Product Description</Form.Label>
+                                        <Form.Control
+                                            type="text"
+                                            placeholder="Enter product description"
+
+                                            onChange={(e) => setProductDescription(e.target.value)}
+                                        />
+                                    </Form.Group>
+                                    <Form.Group className="mb-3">
+                                        <Form.Label>Product Price</Form.Label>
+                                        <Form.Control
+                                            type="number"
+                                            placeholder="Enter product price"
+
+                                            onChange={(e) => setProductPrice(e.target.value)}
+                                        />
+                                    </Form.Group>
+                                    {/* Add more form fields for other product properties... */}
+                                    <Button variant="primary" onClick={handleCreateProduct}>
+                                        Create Product
+                                    </Button>
+                                </Form>
                             </Offcanvas.Body>
                         </Offcanvas>
                     </div>
-                    <div style={{ display: "flex", width: "800px" }}>
+                    <div style={{ display: "flex", maxWidth: "800px", flexWrap: "wrap", height: "fit-content", border: "1px solid black", }}>
                         {props.clickedItem.products && props.clickedItem.products.length > 0 ? (
                             props.clickedItem.products.map(product => (
-                                <Card key={product.id} style={{ width: '14rem' }}>
+                                <Card key={product.id} style={{ maxWidth: '14rem', margin: "8px" }}>
                                     <Card.Img src="https://www.bing.com/th?id=OP.jEg7o%2fYqJQnaQw474C474&o=5&pid=21.1&w=130&h=176&qlt=100&dpr=1.5&bw=6&bc=FFFFFF&c=17" style={{ height: "230px" }} />
                                     <Card.Body>
                                         <Card.Title style={{ textAlign: "center" }}>{product.name}</Card.Title>
@@ -80,8 +227,9 @@ function CategoryPage(props) {
                                             {product.description}
                                         </Card.Text>
                                         <div style={{ display: "flex", justifyContent: "space-evenly" }}>
-                                            <Button variant="primary">Delete</Button>
-                                            <Button variant="primary">Update</Button>
+                                            <Button onClick={() => handleDeleteRequest(product.id)} variant="primary">Delete</Button>
+                                          
+                                            <Button variant="primary" onClick={() => handleUpdateProductId(product.id)}>Update</Button>
                                         </div>
                                     </Card.Body>
                                 </Card>
